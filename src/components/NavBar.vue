9ae1c9a07e82455fd2e2e1c0e8376c2d99e58cf7
ayabenjelloun <147836916+Benaya2005@users.noreply.github.com>
2025-04-26 13:35:28 +0000

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,10 @@
     <nav class="navbar">
       <h1 class="logo">DevGate</h1>
       <router-link to="/Users" >
-      <button v-if="user">Retour</button>
+      <button v-if="user">Users</button>
+    </router-link>
+    <router-link to="/Principale" >
+      <button v-if="user">Dashboard</button>
     </router-link>
       <div v-if="user">
         <button @click="logout" class="btn-logout">Déconnecter</button>
