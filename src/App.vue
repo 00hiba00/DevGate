25f4933d79120c00f9c41ed2828b514bc07b2c5e
00hiba00 <hibaelbouanani123@gmail.com>
2025-05-04 22:27:41 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
     <Navbar v-if="showNavbar" />
-    <router-view />
+    
+    <!-- Show Login component only if on /Login and user is not authenticated -->
+    <Login v-if="route.name === 'Login' && !user" />
+
+    <router-view v-else />
   </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
-import Navbar from '@/components/NavBar.vue'
 import { computed } from 'vue'
+import Navbar from '@/components/NavBar.vue'
+import Login from './views/Login.vue'
+import { useAuth } from './composables/useAuth'
 
 const route = useRoute()
-const showNavbar = computed(() => route.name !== 'Login' && route.name !== 'Register')
+const { user } = useAuth()
+
+const showNavbar = computed(() => {
+  return route.name !== 'Login' && route.name !== 'Register'
+})
 </script>
+
 
 
 <style>
@@ -22,6 +33,7 @@ const showNavbar = computed(() => route.name !== 'Login' && route.name !== 'Regi
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #121212;
 }
 
 nav {
